73d9cf07da9c5c2416e0638436c8ef8ab770a5a2
sb80 <sb80@sanger.ac.uk>
2026-07-02 14:48:31 +0100

    UPDATE: module 2.3 to work with reps 4 and conditions 2

--- modules/p_control_target_implement.py
+++ modules/p_control_target_implement.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from .implement_p_control_indiv_gRNA import implement_p_control_indiv_gRNA
 
-def p_control_target_implement(T_target, T_zGE, binn, p_cont12):
+def p_control_target_implement(T_target, T_zGE, binn, p_cont):
     """
     Given p per bin, infer p/q values for [T_target; T_zGE] from LFC values.
 
@@ -9,10 +9,10 @@ def p_control_target_implement(T_target, T_zGE, binn, p_cont12):
     - T_target: pd.DataFrame with gRNA target data
     - T_zGE: pd.DataFrame with zGE gene data
     - binn: array-like, bin edges
-    - p_cont12: 2D array-like, calibration p/q values per bin (columns for rep1 and rep2)
+    - p_cont: 2D array-like, calibration p/q values per bin
 
     Returns:
-    - T_ij_q_verti: pd.DataFrame with gRNAs, genes, lfc_1, lfc_2, Q1, Q2
+    - T_q_verti: pd.DataFrame with gRNAs, genes, lfc, Q1
     """
 
     #FDR  FRR (false rejection)-correct p-values for both tails of LFC NNT distribution
@@ -22,10 +22,9 @@ def p_control_target_implement(T_target, T_zGE, binn, p_cont12):
     T_target_zGE = pd.concat([T_target, T_zGE], ignore_index=True)
 
     # Extract p/q control columns
-    p_contr1 = p_cont12[:, 0]
-    p_contr2 = p_cont12[:, 1]
+    p_contr = p_cont
 
     # Call the function that assigns p/q values based on LFC bins
-    T_ij_q_verti = implement_p_control_indiv_gRNA(T_target_zGE, binn, p_contr1, p_contr2)
+    T_q_verti = implement_p_control_indiv_gRNA(T_target_zGE, binn, p_contr)
     
-    return T_ij_q_verti
+    return T_q_verti
